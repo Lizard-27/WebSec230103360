4fc888f3ef881c697d177edc98cbeb3391fc7eb2
Lizard-27 <ahmedabdelwhab6836@gmail.com>
2025-03-18 22:05:06 +0200

    2- roles added

--- WebSecService/resources/views/layouts/master.blade.php
+++ WebSecService/resources/views/layouts/master.blade.php
@@ -14,19 +14,44 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto">
+        <ul class="navbar-nav me-auto">
           <li class="nav-item">
             <a class="nav-link" href="{{ url('/') }}">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="{{ url('/register') }}">Register</a>
+            <a class="nav-link" href="{{ url('/even') }}">Even Numbers</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="{{ url('/login') }}">Login</a>
+            <a class="nav-link" href="{{ url('/prime') }}">Prime Numbers</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="{{ url('/logout') }}">Logout</a>
+            <a class="nav-link" href="{{ url('/multable') }}">Multiplication Table</a>
           </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('products_list') }}">Products</a>
+          </li>
+          @can('show_users')
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('users') }}">Users</a>
+          </li>
+          @endcan
+        </ul>
+        <ul class="navbar-nav">
+          @auth
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('profile') }}">{{ auth()->user()->name }}</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('do_logout') }}">Logout</a>
+          </li>
+          @else
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('login') }}">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ route('register') }}">Register</a>
+          </li>
+          @endauth
         </ul>
       </div>
     </div>
