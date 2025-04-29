724c1c62681c1d2be95eab806e6de753b8688fc4
Lizard-27 <ahmedabdelwhab6836@gmail.com>
2025-04-29 11:05:23 +0300

    3- added sql injection

--- WebSecService/routes/web.php
+++ WebSecService/routes/web.php
@@ -53,6 +53,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Route::get('/sqli', function (Request $request) {
+//     $table = $request->'query('table');
+//     DB::unprepared->("DROP TABLE $table")
+//     return redirect('/');
+// });
+
+Route::get('/collect', function (Request $request){
+    $name = $request->query('name');
+    $credit = $request->query('credit');
+
+    return response('data collected',200)
+        ->header('Access-Control-Allow-Orgin', '*')
+        ->header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+        ->header('Access-Control-Allow-Headers', 'Content-Type,X-Requested-With');
+});
+
 Route::get('/multable', function (Request $request) {
     $j = $request->number??5;
     $msg = $request->msg;
